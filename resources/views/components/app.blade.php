1a56b359e6a81ade8a67595776f29839d57ccf84
drapionscorp55 <zellerbustamante@gmail.com>
2024-05-01 15:23:19 +0800

    message

---
+++ resources/views/components/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio 1</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <script src="https://kit.fontawesome.com/377fd11c1f.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div id="header" class="container">
+        {{-- ----- Navigation Bar ---- --}}
+        <nav>
+            <img src="images/logo.png" class="logo">
+            <ul id="sidemenu">
+                <li><a href="#hero">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <i class="fa-solid fa-xmark" onclick="closemenu()"></i>
+            </ul>
+            <i class="fa-solid fa-bars" onclick="openmenu()"></i>
+        </nav>
+    </div>
+
+    <main>
+        {{ $slot }}
+    </main>
+
+</body>
+
+</html>
