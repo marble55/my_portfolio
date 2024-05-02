18b958ff5b708a1637464195e44850b646e8e224
drapionscorp55 <zellerbustamante@gmail.com>
2024-05-02 13:19:05 +0800

    essage

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfolio 1</title>
+    
     @vite('resources/js/app.js')
-    @vite('resources/css/app.css')
     <script src="https://kit.fontawesome.com/377fd11c1f.js" crossorigin="anonymous"></script>
 </head>
 
