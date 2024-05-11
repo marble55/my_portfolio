3bd755135a109cfc71a6c6f820b9e24a29a3b686
drapionscorp55 <zellerbustamante@gmail.com>
2024-05-11 15:43:09 +0800

    message

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -28,6 +28,7 @@ class IndexController extends Controller
         $portfolios = Portfolio::get();
         $contacts = Contact::first();
         $contactLinks = ContactLink::get();
+        
         return view('portfolio.index', compact(
             'heroSections', 
             'abouts', 
@@ -41,51 +42,4 @@ class IndexController extends Controller
         ));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
